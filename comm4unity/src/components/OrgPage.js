f80942065e4ca125a49e9780903ff4ae2c2aee08
Rktify <rktify@gmail.com>
2023-11-20 15:17:22 +0700

    Fixed forms

--- comm4unity/src/components/OrgPage.js
+++ comm4unity/src/components/OrgPage.js
@@ -1,17 +1,35 @@
 import React from "react";
 import Navbar from "./navbar";
+import Form from "./Form";
 import "../styles/org.css";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 
 function Organization(){
-    return(
-        <div>
-            <Navbar/>
-            <h1 class="head"><span> Organization Form </span></h1>
-            <div class="container2">
-                <iframe class="responsive-iframe" src="https://forms.gle/S3pAUoAyfN57p6pH8"></iframe>
-            </div>
-        </div>
-    )
+    class App extends React.Component {
+        render() {
+            return (
+                <div>
+                    <BrowserRouter>
+                        <Navbar/>
+                    </BrowserRouter>
+                    <h1 class="head"><span> Organization Form </span></h1>
+                    <div id="ff-compose"></div>;
+                </div>
+            )
+         }
+        componentDidMount(){
+            var script = document.createElement("script");
+            script.id = "ff-script";
+            script.src = "https://formfacade.com/include/115935186144550186263/form/1FAIpQLSd2ptioKCyRu19d7XW6JdnT9ilq5iHL6_HZ3g7NkmAoGFJ_iA/classic.js?div=ff-compose";
+            script.defer = true;
+            script.async = true;
+            document.body.appendChild(script);
+        }
+    }
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(<App />);
 }
 
-export default Organization
+export default Organization;
+
