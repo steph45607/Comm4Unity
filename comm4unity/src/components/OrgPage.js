3d1b7e77933d990ff76d79055539816797ff8cc6
Rktify <rktify@gmail.com>
2023-11-20 14:28:35 +0700

    iframe form

--- comm4unity/src/components/OrgPage.js
+++ comm4unity/src/components/OrgPage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./navbar";
+import "../styles/org.css";
 
 function Organization(){
     return(
         <div>
             <Navbar/>
-            <p>Organization</p>
+            <h1 class="head"><span> Organization Form </span></h1>
+            <div class="container2">
+                <iframe class="responsive-iframe" src="https://forms.gle/S3pAUoAyfN57p6pH8"></iframe>
+            </div>
         </div>
     )
 }
