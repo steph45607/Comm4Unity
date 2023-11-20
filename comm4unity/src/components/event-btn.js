b4c0c9584b43a53e7ee865f4f1737f3fbc08094a
steph45607 <staniswinata.stephanie@gmail.com>
2023-11-20 14:25:58 +0700

    event button fixed withh image

--- comm4unity/src/components/event-btn.js
+++ comm4unity/src/components/event-btn.js
@@ -2,17 +2,16 @@ import React from "react";
 import "../styles/eventBtn.css";
 
 function EventBtn({ title, reward, org, poster }) {
-  return (<div>
-
-    <div className="event-container" style={{backgroundImage:`url(${poster})`}}>
-    </div>
-      <div className="event-text">
+  return (
+  <div className="event-btn-container">
+    <div className="event-container" style={{backgroundImage:`url(${poster})`}}></div>
+    <div className="event-text">
         <p>{title}</p>
         <div className="org-reward">
-          <p>{org}</p>
-          <p className="reward">{reward}</p>
+            <p>{org}</p>
+            <p className="reward">{reward}</p>
         </div>
-      </div>
+    </div>
   </div>
   );
 }
