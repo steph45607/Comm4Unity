34c9d4c4f693b15ffe38c19c1946251f2019e3c0
steph45607 <staniswinata.stephanie@gmail.com>
2024-01-09 22:34:12 +0700

    event page from db

--- comm4unity/src/components/Event.js
+++ comm4unity/src/components/Event.js
@@ -6,42 +6,59 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Button from "./button.js";
+import { event_url } from "./CONST.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function Event() {
+const Event = () => {
   const { id } = useParams();
+  const [event, setEvent] = useState("");
+  const fetchEventData = async () => {
+    try {
+      // const userUid = user?.uid;
+      console.log(id);
 
-  const selectedEvent = events.find((event) => event.id === id);
+      const event_data = await axios.get(`${event_url}/event/${id}`);
+      console.log(event_data);
+      setEvent(event_data.data);
+      //   setComserv(student.data)
+    } catch (error) {
+      console.error("Error fetching student details:", error.message);
+    }
+  };
 
-  if (!selectedEvent) {
-    return <div>Event not found</div>;
-  }
+  useEffect(()=>{
+    fetchEventData();
+  });
+
   return (
     <div>
+      {/* {event.id} */}
       <div className="top">
         <Link to={"/student"} style={{textDecoration:"none", color:"white"}}>
           <FontAwesomeIcon icon={faArrowLeft} size="2xl" />
         </Link>
-        <h2 className="event-title">{selectedEvent.title}</h2>
+        <h2 className="event-title">{event.title}</h2>
       </div>
       <div className="event-container">
         <div className="event-left">
-          <img className="event-image" src={selectedEvent.poster} alt=""></img>
+          <img className="event-image" src={event.image_link} alt=""></img>
           <p className="eventp">
-            {selectedEvent.date} | {selectedEvent.time}
+            {event.date} | {event.start}
           </p>
-          <p className="eventp">Location: {selectedEvent.location}</p>
+          <p className="eventp">Location: {event.location}</p>
           <div className="event-btn">
-            <a href={selectedEvent.link} target="_blank">
+            <a href={event.regist_link} target="_blank">
                 <p className="eventp">Apply</p>
             </a>
           </div>
         </div>
         <div className="event-right">
-          <p className="eventp">{selectedEvent.details}</p>
+          <p className="eventp">{event.description}</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Event;
